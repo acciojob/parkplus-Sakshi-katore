d683b22c5342971750bfbe635241e5ea768ca2f1
Sakshi Katore <katoresakshi@gmail.com>
2024-05-05 00:04:07 +0530

    a

--- src/main/java/com/driver/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/driver/services/impl/PaymentServiceImpl.java
@@ -1,6 +1,8 @@
 package com.driver.services.impl;
 
+import com.driver.model.Payment;
 import com.driver.model.PaymentMode;
+import com.driver.model.Reservation;
 import com.driver.repository.PaymentRepository;
 import com.driver.repository.ReservationRepository;
 import com.driver.services.PaymentService;
@@ -16,6 +18,37 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception {
+        Reservation reservation=reservationRepository2.findById(reservationId).get();
 
+        int TotHour=reservation.getNumberOfHours();
+        int pricePerHour=reservation.getSpot().getPricePerHour();
+
+        int bill=TotHour*pricePerHour;
+
+        if(amountSent<bill){
+            throw new Exception("Insufficient Amount");
+        }
+
+        if(!mode.equalsIgnoreCase("cash") && !mode.equalsIgnoreCase("card") && !mode.equalsIgnoreCase("upi")){
+            throw new Exception("Payment mode not detected");
+        }
+
+        Payment payment=new Payment();
+        payment.setPaymentCompleted(true);
+
+        if(mode.equalsIgnoreCase("cash")){
+            payment.setPaymentMode(PaymentMode.CASH);
+        }else if(mode.equalsIgnoreCase("card")){
+            payment.setPaymentMode(PaymentMode.CARD);
+        }else{
+            payment.setPaymentMode(PaymentMode.UPI);
+        }
+        payment.setReservation(reservation);
+        reservation.setPayment(payment);
+
+//        payment=paymentRepository2.save(payment);
+        reservationRepository2.save(reservation);
+
+        return payment;
     }
 }
--- src/main/java/com/driver/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/driver/services/impl/ReservationServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReservationServiceImpl implements ReservationService {
@@ -23,6 +24,96 @@ public class ReservationServiceImpl implements ReservationService {
     ParkingLotRepository parkingLotRepository3;
     @Override
     public Reservation reserveSpot(Integer userId, Integer parkingLotId, Integer timeInHours, Integer numberOfWheels) throws Exception {
+        Optional<User> userOptional=userRepository3.findById(userId);
 
+        if(userOptional==null){
+            throw new Exception("Cannot make reservation" );
+        }
+        User user=userOptional.get();
+
+//      ParkingLot parkingLot=parkingLotRepository3.findById(parkingLotId).get();
+        Optional<ParkingLot>optionalParkingLot=parkingLotRepository3.findById(parkingLotId);
+
+        if(optionalParkingLot==null){
+            throw new Exception("Cannot make reservation");
+        }
+
+        ParkingLot parkingLot=optionalParkingLot.get();
+
+        List<Spot>spotList=parkingLot.getSpotList();
+
+        Spot spot=null;
+        int minP=Integer.MAX_VALUE;
+
+
+        for(Spot spot1:spotList){
+            if(spot1.getOccupied()==false){
+
+                Integer noOfVehicle=0;
+
+                if(spot1.getSpotType()== SpotType.TWO_WHEELER){
+                    noOfVehicle=2;
+                }else if(spot1.getSpotType()== SpotType.FOUR_WHEELER){
+                    noOfVehicle=4;
+                }else{
+                    noOfVehicle=numberOfWheels;
+                }
+
+                if(noOfVehicle>=numberOfWheels && minP>spot1.getPricePerHour()){
+                    minP=spot1.getPricePerHour();
+                    spot=spot1;
+                }
+            }
+        }
+
+        if(spot==null){
+            throw new Exception("Cannot make reservation");
+        }
+
+//      List<Spot>spotList=spotRepository3.findAllSpot();
+//      Spot spot=null;
+//      for(Spot spot1:spotList){
+//          if(spot1.getOccupied()==false){
+//              Integer noOfVehicle=0;
+//
+//              if(spot1.getSpotType()== TWO_WHEELER){
+//                   noOfVehicle=2;
+//              }else if(spot1.getSpotType()== FOUR_WHEELER){
+//                  noOfVehicle=4;
+//              }else{
+//                  noOfVehicle=5;
+//              }
+//
+//              if(numberOfWheels<=noOfVehicle){
+//                  spot=spot1;
+//              }
+//          }
+//      }
+//
+//
+//       if(spot==null){
+//           throw new Exception("Cannot make reservation");
+//       }
+
+//        assert spot!= null;
+
+        Reservation reservation=new Reservation();
+        reservation.setNumberOfHours(timeInHours);
+        reservation.setSpot(spot);
+        reservation.setUser(user);
+
+        spot.setOccupied(true);
+
+        spot.getReservationList().add(reservation);
+        user.getReservationList().add(reservation);
+
+        spotRepository3.save(spot);
+        userRepository3.save(user);
+
+
+//       reservationRepository3.save(reservation);
+
+
+        return reservation;
     }
 }
